cd1262667697762ab28c2ac638ebf7c4029dfe7b
Leonardo Christ <christ.leonardo21@gmail.com>
2024-06-25 17:46:31 -0300

    entrega do exercicio

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-const alunos = ['Leonardo', 'Rafael', 'Gabriel', 'Mateus', 'Marcos', 'Suéllen', 'Julia', 'Vanessa', 'Eduarda']
+function notaAleatoria() {
+    return Math.floor(Math.random() * 11);
+  }
+
+const alunos = [
+    { Nome: "Sophia", nota: notaAleatoria() },
+    { Nome: "Miguel", nota: notaAleatoria() },
+    { Nome: "Isabella", nota: notaAleatoria() },
+    { Nome: "Gabriel", nota: notaAleatoria() },
+    { Nome: "Helena", nota: notaAleatoria() },
+    { Nome: "Arthur", nota: notaAleatoria() },
+    { Nome: "Valentina", nota: notaAleatoria() },
+    { Nome: "João", nota: notaAleatoria() },
+    { Nome: "Laura", nota: notaAleatoria() },
+    { Nome: "Eduardo", nota: notaAleatoria() }
+  ];
+
+const alunosPassados = alunos.filter((media) => media.nota >= 6)
+
+console.log(alunosPassados)
